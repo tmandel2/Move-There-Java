1c54e485c21e85ff2087dce0d5137fcab933c1ec
Tim Mandel <timothy.mandel@gmail.com>
2019-03-07 11:00:14 -0600

    Address delete and edit

--- src/main/java/movethere/AddressController.java
+++ src/main/java/movethere/AddressController.java
@@ -31,4 +31,46 @@ public class AddressController {
         return address;
     }
 
+    @GetMapping("/addresses")
+    public Iterable<Address> getAddresses(){
+        Iterable<Address> addresses = addressRepository.findAll();
+        return addresses;
+    }
+
+    @GetMapping("/addresses/{id}")
+    public Address showAddress(@PathVariable Long id){
+        Address foundAddress = addressRepository.findById(id).get();
+        return foundAddress;
+    }
+
+    @PutMapping("/addresses/{id}")
+    public Address updateAddress(@RequestBody Address address, @PathVariable Long id) throws Exception{
+        Optional<Address> editedAddress = addressRepository.findById(id);
+        if(editedAddress.isPresent()){
+            Address addressToEdit = editedAddress.get();
+            addressToEdit.setStreetNumber(address.getStreetNumber());
+            addressToEdit.setStreetName(address.getStreetName());
+            addressToEdit.setCity(address.getCity());
+            addressToEdit.setState(address.getState());
+            addressToEdit.setZipCode(address.getZipCode());
+            addressToEdit.setLatitude(address.getLatitude());
+            addressToEdit.setLongitude(address.getLongitude());
+            addressToEdit.setWalkScore(address.getWalkScore());
+            addressToEdit.setDiversity(address.getDiversity());
+            addressToEdit.setMedianAge(address.getMedianAge());
+            addressToEdit.setHouseValue(address.getHouseValue());
+            return addressRepository.save(addressToEdit);
+        } else {
+            throw new Exception("address not here");
+        }
+    }
+
+    @DeleteMapping("/addresses/{id}")
+    public String deleteAddress(@PathVariable Long id){
+        addressRepository.deleteById(id);
+        return "address removed";
+    }
+
+
+
 }
